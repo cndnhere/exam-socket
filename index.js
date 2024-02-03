928f986ef43f56387cf20f924bba5c0aef257f2f
cndnhere <98242648+cndnhere@users.noreply.github.com>
2024-02-03 17:33:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//index.js
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+let userAns=[];
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  
+  console.log('a user connected');
+
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  }); 
+ 
+  
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected!');
+  });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
